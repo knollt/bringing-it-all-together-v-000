8e262c385d8e43a4302c5c6bcb4b427a8c4da304
Tim Knoll <timothy.knoll@gmail.com>
2019-06-01 17:46:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -76,7 +76,7 @@ class Dog
     if dog
       new_dog = self.new_from_db(dog)
     else
-      new_dog = self.create()
+      new_dog = self.create({:name => name, :breed => breed})
     end 
     new_dog
   end   
